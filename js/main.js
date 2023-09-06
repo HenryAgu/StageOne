9de95456c24856471fd30414afdf5cd15aa94522
Henry Agu <aguhenrychuks@gmail.com>
2023-09-06 23:54:53 +0100

    fix: removed seconds and 12  hour

--- js/main.js
+++ js/main.js
@@ -8,28 +8,22 @@ const date = new Date();
 const dayOfTheWeek = date.getDay();
 
 // AM or PM
-// let hour12;
+let hour12;
 
 // Hour of the day 
 const hour = date.getHours();
-if(hour >= 12){
-    nigerianHour = hour - 12;
-}
 
 // AM or PM handler
-// if (hour > 12){
-//     hour12 = "PM";
-// }else if(hour < 12){
-//     hour12 = "AM";
-// }else{
-//     "Inavlid Hour";
-// }
+if (hour > 12){
+    hour12 = "PM";
+}else if(hour < 12){
+    hour12 = "AM";
+}else{
+    "Inavlid Hour";
+}
 
 // Minute of the day
 const minute = date.getMinutes();
-
-// Seconds of the day
-const seconds = date.getSeconds();
 
 // Day DOM element
 const day = document.getElementById("day");
@@ -37,4 +31,4 @@ day.textContent = `${days[dayOfTheWeek]}`
 
 // Time DOM element
 const time = document.getElementById("time");
-time.textContent = `${nigerianHour}:${minute}:${seconds}`
+time.textContent = `${hour}:${minute} ${hour12}`
